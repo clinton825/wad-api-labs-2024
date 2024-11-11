6ce579608c9c8ad34c0019efca0bf7e9e459e0f7
clinton825 <20097793@mail.wit.ie>
2024-11-11 15:53:32 +0000

    node-lab1, simple server

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import react from "eslint-plugin-react";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended", "plugin:react/recommended"), {
+    plugins: {
+        react,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    rules: {
+        semi: 1,
+        "no-console": "off",
+    },
+}];
